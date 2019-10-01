6f9c7f67857d69af330bf7e721c06bf4d90976e3
Samraddhi Khare <samriddhi.khare42@gmail.com>
2019-10-01 11:37:15 -0700

    added one statement

--- SimpleClassMappingMaven/src/test/java/com/zensar/hibernate/HibernateMain.java
+++ SimpleClassMappingMaven/src/test/java/com/zensar/hibernate/HibernateMain.java
@@ -7,25 +7,30 @@ import org.hibernate.cfg.Configuration;
 
 import com.zensar.hibernate.entities.Product;
 
-public class HibernateMain {
-public static void main(String[] args) {
-Configuration c= new Configuration().configure();
-SessionFactory f=c.buildSessionFactory();
-Session s= f.openSession();
-Transaction t= s.beginTransaction();
+public class HibernateMain
+{
+	
+public static void main(String[] args)
+{
+	
+ Configuration c= new Configuration().configure();
+ SessionFactory f=c.buildSessionFactory();
+ Session s= f.openSession();
+ Transaction t= s.beginTransaction();
 
-Product p= new Product();
-p.setProductId(1001);
-p.setName("Led Tv");
-p.setBrand("sony");
-p.setPrice(30000);
+ Product p= new Product();
+ p.setProductId(1001);
+ p.setName("Led Tv");
+ p.setBrand("sony");
+ p.setPrice(30000);
 
-s.save(p);
+ s.save(p);
+ System.out.println("Product is saved");
 
 
 
-t.commit();
-s.close();
+  t.commit();
+  s.close();
 }
 
 }
